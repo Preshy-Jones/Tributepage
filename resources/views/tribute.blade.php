3f510269d691d075fb1fe0c8260bf4b5328bd73f
Preshy-Jones <adedibuprecious@gmail.com>
2020-06-18 21:56:27 +0100

    tribute app

---
+++ resources/views/tribute.blade.php
@@ -0,0 +1,76 @@
+<html>
+
+<head>
+  <script src="https://cdn.freecodecamp.org/testable-projects-fcc/v1/bundle.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <title>Goku</title>
+</head>
+
+<body>
+  <div id="main">
+    <div id="title">
+      <h1>500 DAYS OF SUMMER</h1>
+      <p id="pp">Not your typical Romatic comedy-drama</p>
+    </div>
+     <div id="box-1">
+       <div id="img-div">
+         <p id="img-caption">Tom in one of his gloomy days of summer.</p>
+         <p id="aboutgokubuttonhref">.</p>
+       <img  id="image" alt="Goku Base Form" src="{{asset('img/500dos.jpg')}}">
+         </div>
+        </div>
+       <div id="tribute-info">
+         <h2>Tribute</h2>
+         <p><strong>500 days of summer</strong> is movie ahead of its time. It does not exclude itself from the annoying cliches of 
+            romantic films but rather it challenges them.  It challenges the love at first sight BS. The film lowers your expectations up front that it is 
+            not a love story but still makes you wishing it was through out the movie. It plays around the role fate plays
+            in two individuals falling in love together. There are two warring themes in the movie. On one corner you have the "soulmate" ideology that there is someone out
+            who was destined to be with you,who likes the same things you like and is just the perfect match for you. On the other corner you have the ideology that there is no such
+            as love all together,its just fantasy and there is no destined perfect person for everyone. 
+        </p>
+        <p>At the end it never really decides a clear winner. Rather it gives the feeling that there is something valuable to learn
+         from both these two ideologies. That love exists but there isn't a perfect person who would agree with everything with you and like 
+         exactly the same things you like. Its a movie that would end up as a classic and it has had a great cultural impact. People connect to how realistic the
+         movie is. Its referenced several time across pop culture usually as that one realistic romantic film.            
+        </p> 
+        <h2>Music</h2>
+        <p>500 days of summer has the best collection of music and soundtracks i have ever seen in a film. The music was just well chosen.
+            Each track fit every scene it covered and perfectly conveyed the emotions of every scene. My favorite of the tracks were from the very talented Russian born American singer 
+            Regina Spektor. The first was her song "Us" played at the beginning of the film and perfectly conveyed the nostaligic feeling the scene was going for.
+            The second was "hero" which just perfect as it was played at a major turning point in the movie. 
+        </p>
+        <section id="tribute-inf">
+            <h2 class="headline">
+                About 500 days of summer
+            </h2>
+            <ul>
+                <li>
+                    <p><strong> Released:</strong> January 17, 2009(United States)</p>
+                </li>
+                <li>
+                    <p><strong>Starring:</strong> 	
+                        Joseph Gordon-Levitt
+                        Zooey Deschanel</p>
+                </li>
+                <li>
+                    <p><strong>Distributed by:</strong>	Fox Searchlight Pictures</p>
+                </li>
+                <li>
+                    <p><strong>Directed by:</strong>	Marc Webb
+                    <p>
+                </li>
+                <li>
+                    <p><strong>Running time:</strong> 95 minutes</p>
+                </li>
+
+            </ul>
+
+        </div>
+
+     </div>
+</body>
+
+</html>
+
+
